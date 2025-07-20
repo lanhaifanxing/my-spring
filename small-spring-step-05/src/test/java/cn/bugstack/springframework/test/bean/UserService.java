61470967b99b6d2af23b8b8f799b70359ca64b8b
lishuaipeng <lishuaipeng2024@163.com>
2025-07-20 10:52:22 +0800

    资源加载器解析文件注册对象

---
+++ small-spring-step-05/src/test/java/cn/bugstack/springframework/test/bean/UserService.java
@@ -0,0 +1,30 @@
+package cn.bugstack.springframework.test.bean;
+
+public class UserService {
+
+    private String uId;
+
+    private UserDao userDao;
+
+    public String queryUserInfo() {
+        System.out.println("查询用户信息：" + userDao.queryUserName(uId));
+        return userDao.queryUserName(uId);
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+}
