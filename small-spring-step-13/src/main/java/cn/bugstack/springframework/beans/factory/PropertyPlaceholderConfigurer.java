80f7995dc1fb52ed0f60e1e8a795c7675fcd3fbe
lishuaipeng <lishuaipeng2024@163.com>
2025-08-18 22:36:47 +0800

    自动扫描Bean对象注册

---
+++ small-spring-step-13/src/main/java/cn/bugstack/springframework/beans/factory/PropertyPlaceholderConfigurer.java
@@ -0,0 +1,70 @@
+package cn.bugstack.springframework.beans.factory;
+
+import cn.bugstack.springframework.beans.BeansException;
+import cn.bugstack.springframework.beans.PropertyValue;
+import cn.bugstack.springframework.beans.PropertyValues;
+import cn.bugstack.springframework.beans.factory.config.BeanDefinition;
+import cn.bugstack.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import cn.bugstack.springframework.core.io.DefaultResourceLoader;
+import cn.bugstack.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyPlaceholderConfigurer implements BeanFactoryPostProcessor {
+
+    /**
+     * Default placeholder prefix: {@value}
+     */
+    public static final String DEFAULT_PLACEHOLDER_PREFIX = "${";
+
+    /**
+     * Default placeholder suffix: {@value}
+     */
+    public static final String DEFAULT_PLACEHOLDER_SUFFIX = "}";
+
+    private String location;
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        // 加载属性文件
+        try {
+            DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
+            Resource resource = resourceLoader.getResource(location);
+            Properties properties = new Properties();
+            properties.load(resource.getInputStream());
+
+            String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
+            for (String beanName : beanDefinitionNames) {
+                BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanName);
+                //遍历每个bean，查看bean中是否
+                PropertyValues propertyValues = beanDefinition.getPropertyValues();
+                for (PropertyValue propertyValue : propertyValues.getPropertyValues()) {
+                    Object value = propertyValue.getValue();
+                    if (!(value instanceof String)) continue;
+                    String strVal = (String) value;
+                    StringBuilder buffer = new StringBuilder(strVal);
+                    //如果没有占位符，index为-1.判断bean中是否有需要取属性的
+                    int startIdx = strVal.indexOf(DEFAULT_PLACEHOLDER_PREFIX);
+                    int stopIdx = strVal.indexOf(DEFAULT_PLACEHOLDER_SUFFIX);
+                    //替换占位符为属性文件中的属性
+                    if (startIdx != -1 && stopIdx != -1 && startIdx < stopIdx) {
+                        String propKey = strVal.substring(startIdx + 2, stopIdx);
+                        //从配置文件中找占位符中的变量，然后替换
+                        String propVal = properties.getProperty(propKey);
+                        buffer.replace(startIdx, stopIdx + 1, propVal);
+                        propertyValues.addPropertyValue(new PropertyValue(propertyValue.getName(), buffer.toString()));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new BeansException("Could not load properties", e);
+        }
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
+
---
+++ small-spring-step-13/src/test/java/cn/bugstack/springframework/test/ApiTest.java
@@ -0,0 +1,41 @@
+package cn.bugstack.springframework.test;
+
+import cn.bugstack.springframework.aop.AdvisedSupport;
+import cn.bugstack.springframework.aop.MethodMatcher;
+import cn.bugstack.springframework.aop.TargetSource;
+import cn.bugstack.springframework.aop.aspectj.AspectJExpressionPointcut;
+import cn.bugstack.springframework.aop.framework.Cglib2AopProxy;
+import cn.bugstack.springframework.aop.framework.JdkDynamicAopProxy;
+import cn.bugstack.springframework.aop.framework.ReflectiveMethodInvocation;
+import cn.bugstack.springframework.context.support.ClassPathXmlApplicationContext;
+import cn.bugstack.springframework.test.bean.IUserService;
+import cn.bugstack.springframework.test.bean.UserService;
+import cn.bugstack.springframework.test.bean.UserServiceInterceptor;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ */
+public class ApiTest {
+    @Test
+    public void test_property() {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-property.xml");
+        IUserService userService = applicationContext.getBean("userService", IUserService.class);
+        System.out.println("测试结果：" + userService);
+    }
+
+    @Test
+    public void test_scan() {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-scan.xml");
+        IUserService userService = applicationContext.getBean("userService", IUserService.class);
+        System.out.println("测试结果：" + userService.queryUserInfo());
+    }
+
+
+
+}
